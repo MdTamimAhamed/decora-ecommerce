96a7822f0972b209ca5c133f8ab11ec64edda15d
Tamim Ahamed <tamimssgts999@gmail.com>
2024-09-07 03:53:17 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.use(
 	cors({
 		origin: 'https://decora-ecommerce-client.vercel.app',
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		 allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 		credentials: true,
 	})
 );
