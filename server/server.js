3b4f1cf969897fb242ba49b76e0faf852dbb1839
MdTamimAhamed <tamimssgts999@gmail.com>
2024-09-07 02:50:07 +0600

    vercel config added and baseURL

--- server/server.js
+++ server/server.js
@@ -18,27 +18,41 @@ const port = process.env.PORT;
 
 //db
 const database = mongoose
-	.connect(process.env.CONNECTION_STRING)
+	.connect(
+		'mongodb+srv://database_tamim:DB.24_zan_cluster0.1@cluster0.ojxut.mongodb.net/'
+	)
 	.then(() => {
-		console.log('Databased connected!');
+		console.log('Database connected!');
 	})
 	.catch((err) => {
 		console.log(err);
 	});
 
+app.get('/', (req, res) => {
+	res.json('Hello');
+});
+
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			'https://decora-ecommerce-r8329v81b-mdtamimahameds-projects.vercel.app',
+		],
+		methods: ['POST', 'GET'],
+		credentials: true,
+	})
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //routes
-app.use('/customer', customerRoutes);
-app.use('/seller', sellerRoutes);
-// app.use('/api', authRoutes);
+// app.use('/customer', customerRoutes);
+// app.use('/seller', sellerRoutes);
+// // app.use('/api', authRoutes);
 
-//error handle
-app.use(notFoundErrorHandler);
-app.use(defaultErrorHandler);
+// //error handle
+// app.use(notFoundErrorHandler);
+// app.use(defaultErrorHandler);
 
 //listen
 app.listen(process.env.PORT, () => {
